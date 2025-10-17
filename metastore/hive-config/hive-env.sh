e02934b7439d183b6aa6bc2aee5f3ab5aa0943b5
Dhoa0102 <hoa856856@gmail.com>
2025-10-17 15:25:14 +0700

    feat: ETL, Visualization

--- metastore/hive-config/hive-env.sh
+++
@@ -1,7 +0,0 @@
-# minimal hive-env.sh
-export HADOOP_HOME=/opt/hadoop
-export HADOOP_CONF_DIR=/etc/hadoop/conf
-export HIVE_CONF_DIR=/opt/hive/conf
-export HIVE_LOG_DIR=/opt/hive/logs
-# If you want to add JVM args, use a named var:
-export HADOOP_CLIENT_OPTS="${HADOOP_CLIENT_OPTS:-} -Dproc_metastore"
